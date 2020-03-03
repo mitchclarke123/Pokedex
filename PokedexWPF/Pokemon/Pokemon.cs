7298ec763d5fe21e367f34f96d59bb261cb49c72
mitchclarke123 <mitchclarke123@hotmail.com>
2020-03-03 18:22:40 +0000

    first commit, set up basics for project

---
+++ PokedexWPF/Pokemon/Pokemon.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pokemon
+{
+    public class Pokemon
+    {
+    }
+}
---
+++ Pokemon create Query 1.sql
@@ -0,0 +1,26 @@
+--DROP DATABASE 
+
+--CREATE DATABASE POKEDEXDB
+
+--DROP TABLE Pokemon
+
+--CREATE TABLE Pokemon
+--(
+-- PokemonID INT NOT NULL IDENTITY (1,1) PRIMARY KEY
+--,PokemonName VARCHAR(30)
+--);
+
+
+--CREATE TABLE PokemonImage
+--(
+--PokemonID INT
+--FOREIGN KEY (PokemonID) REFERENCES Pokemon (PokemonID)
+--,PokemonImage VARBINARY(MAX) 
+--);
+
+
+--INSERT [Pokemon]
+--		([PokemonName])
+--		VALUES
+--		('Bulbasaur')
+--;
